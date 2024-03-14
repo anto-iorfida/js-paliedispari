7444fdea5fea6d25d6be07e07201139a00870bbf
anto-iorfida <antonioiorfida@hotmail.com>
2024-03-14 16:23:56 +0100

    feat created function verify palidrome

---
+++ palidroma/js/script.js
@@ -0,0 +1,29 @@
+// Palidroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+// chiedere all'utente di inserire una parola 
+const userWord =  'miau' //prompt('digita una parola per sapere se è palindroma')
+console.log(userWord)
+
+
+
+// FUNZIONE 
+// creare una funzione per capire se una parola è PALINDROMA
+// word-->parola da verificare
+function verifyPalindrome (word){
+    // inanzitutti bisogna lavorare su ogni singola lettera 
+    // e salvare la parola al contrario nella variabile wordVerify 
+    let wordVerify = '' ;
+    for(let i = userWord.length - 1 ; i >= 0 ;i--){
+        wordVerify += userWord[i] ;
+    }
+    // creare condizione che se la parola utente è uguale alla parola al contrario allora la parola e palindroma 
+    message;
+    if(userWord === wordVerify){
+        message = 'la parola è PALIDROMA'
+    }else {
+        message = 'la parola non è PALIDROMA'
+    }
+    
+}
